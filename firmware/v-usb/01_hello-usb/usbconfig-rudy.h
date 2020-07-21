f19cfe5b94a28862c03518b538d853e7f22be805
Sven Gregori <svengregori@gmail.com>
2020-07-21 16:37:37 +0300

    FW: moved examples and refactored V-USB config override handling
    V-USB is great, but even though this is specifically a Random *USB*
    Device, it doesn't have to be, and using V-USB functionality is really
    more optional, and is therefore now separated.
    
    Not that there's much to separate it from, but now it's easier to add
    bare metal examples in the future.
    
    While moving the examples, the config handling for V-USB was refactored
    along the way. It didn't make sense anymore to explicitly undef the
    values to override them. Instead, the main usbconfig.h uses values set
    in an usbconfig-rudy.h override header file that can be adjusted for
    each example project individually.

---
+++ firmware/v-usb/01_hello-usb/usbconfig-rudy.h
@@ -0,0 +1,42 @@
+#ifndef RUDY_USBCONFIG_H
+#define RUDY_USBCONFIG_H
+
+
+#define RUDY_IMPLEMENT_FN_READ 0
+#define RUDY_IMPLEMENT_FN_WRITE 0
+#define RUDY_IMPLEMENT_FN_WRITEOUT 0
+
+#define RUDY_VENDOR_ID  0x09, 0x12 // -> 0x1209
+#define RUDY_DEVICE_ID  0x0b, 0xb0 // -> 0xb00b
+
+// comment out to not have one
+#define RUDY_VENDOR_NAME    'C', 'r', 'a', 'p', 'L', 'a', 'b'
+#define RUDY_VENDOR_NAME_LEN 7
+
+// comment out to not have one
+#define RUDY_DEVICE_NAME    'R', 'U', 'D', 'Y'
+#define RUDY_DEVICE_NAME_LEN 4
+
+// comment out to not have one
+//#define RUDY_SERIAL_NUMBER 'N', 'o', 'p', 'e'
+//#define RUDY_SERIAL_NUMBER_LEN 4
+
+#define RUDY_DEVICE_CLASS 0xff
+#define RUDY_DEVICE_SUBCLASS 0x00
+
+#define RUDY_INTERFACE_CLASS     0
+#define RUDY_INTERFACE_SUBCLASS  0
+#define RUDY_INTERFACE_PROTOCOL  0
+
+#define RUDY_DESCR_PROPS_DEVICE                  0
+#define RUDY_DESCR_PROPS_CONFIGURATION           0
+#define RUDY_DESCR_PROPS_STRINGS                 0
+#define RUDY_DESCR_PROPS_STRING_0                0
+#define RUDY_DESCR_PROPS_STRING_VENDOR           0
+#define RUDY_DESCR_PROPS_STRING_PRODUCT          0
+#define RUDY_DESCR_PROPS_STRING_SERIAL_NUMBER    0
+#define RUDY_DESCR_PROPS_HID                     0
+#define RUDY_DESCR_PROPS_HID_REPORT              0
+#define RUDY_DESCR_PROPS_UNKNOWN                 0
+
+#endif /* RUDY_USBCONFIG_H */
---
+++ firmware/v-usb/common/common.mk
@@ -0,0 +1,100 @@
+#
+# RUDY - the Random USB Device
+# Common Makefile definitions for V-USB examples
+#
+# Copyright (C) 2020 Sven Gregori <sven@craplab.fi>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+MCU = atmega328p
+F_CPU = 12000000
+
+
+# location to common files (i.e. the directory this common.mk file
+# here is located) relative to the example project source files.
+# If not defined in the project's Makefile, fall back to ../common
+COMMON ?= ../common
+
+USBDRV_SRC = $(COMMON)/usbdrv/usbdrv.c
+USBDRV = usbdrv.o
+USBDRVASM_SRC = $(COMMON)/usbdrv/usbdrvasm.S
+USBDRVASM = usbdrvasm.o
+
+USBCONFIG = usbconfig-rudy.h
+
+ifeq (,$(wildcard $(USBCONFIG)))
+USBCONFIG = $(COMMON)/usbconfig-rudy.h
+endif
+
+BUILD_OBJS = $(OBJS) $(USBDRV) $(USBDRVASM)
+
+
+CC = avr-gcc
+OBJCOPY = avr-objcopy
+SIZE = avr-size
+AVRDUDE = avrdude
+
+CFLAGS += -g -Os -std=gnu99 -I. -I$(COMMON)/\
+-funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums \
+-Wall -Wextra -Wstrict-prototypes \
+-DF_CPU=$(F_CPU) -mmcu=$(MCU) 
+
+ASFLAGS = -Wa,-adhlms=$(<:.c=.lst),-gstabs 
+ASFLAGS_ASM = -Wa,-gstabs 
+LDFLAGS = -Wl,-Map=$(PROGRAM).map,--cref
+AVRDUDE_FLAGS = -p $(MCU) -c usbasp
+
+
+.PRECIOUS : %.elf %.o
+
+all: $(PROGRAM).hex
+
+$(PROGRAM).hex: $(PROGRAM).elf
+	$(OBJCOPY) -O ihex -R .eeprom $< $@
+	@$(SIZE) $^
+
+$(PROGRAM).elf: $(BUILD_OBJS)
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
+
+.c.o: $(USBCONFIG)
+	$(CC) -c $(CFLAGS) $(ASFLAGS) $< -o $@
+
+$(USBDRV): $(USBDRV_SRC) $(USBCONFIG)
+	$(CC) -c $(CFLAGS) $< -o $@
+
+$(USBDRVASM): $(USBDRVASM_SRC) $(USBCONFIG)
+	$(CC) -c $(CFLAGS) -x assembler-with-cpp $(ASFLAGS_ASM) $< -o $@
+
+burn-fuse:
+	# External full swing crystal oscillator, 16K CK / 14 CK + 65ms
+	$(AVRDUDE) $(AVRDUDE_FLAGS) -U lfuse:w:0xf7:m -U hfuse:w:0xd9:m -U efuse:w:0xff:m
+
+program:
+	$(AVRDUDE) $(AVRDUDE_FLAGS) -U flash:w:$(PROGRAM).hex
+
+clean:
+	rm -f $(BUILD_OBJS)
+	rm -f $(OBJS:.o=.lst)
+
+distclean: clean
+	rm -f $(PROGRAM).elf $(PROGRAM).hex $(PROGRAM).map
+
+.PHONY : all clean distclean program
+
